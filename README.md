54fa51c900c0fd8044150c781da5c7439a6e2cbe
iawall <iawall@siue.edu>
2024-01-31 14:51:04 -0600

    worked on scrap.py, added to README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-1/24:
-    Created Repository, linked to VScode
+
+Firstly, I created my repository in git, linked it to VScode, installed packages request (used to retrieve the website) and BeautifulSoup4 (used for the web scraping) to my environment
+
+After doing this, I then pathed my VScode to the miniconda environment so that the code i run in VScode uses the packages within my conda environment.
+
+I then created my scraping python program:
+  --  scrap.py explanation soon --
+
+
+
--- scrap.py
+++ scrap.py
@@ -1,16 +1,25 @@
-import requests
-from bs4 import BeautifulSoup
+import requests                 # Makes HTTP requests
+from bs4 import BeautifulSoup   # Web scraping
 
-url = 'https://www.scrapethissite.com/pages/simple/'
-response = requests.get(url)
+url = 'https://www.scrapethissite.com/pages/simple/' # name of the site to scrape
+response = requests.get(url)    # Sends a GET request to the website
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    headlines = soup.find_all('h1')  # Assuming headlines are in h2 tags
-
+if response.status_code == 200: # successful retrieval request
+    soup = BeautifulSoup(response.text, 'html.parser') # Parsing the content of the page
+    
+    headlines = soup.find_all('h1')  # Assuming headlines are in h1 tags
     print("Headlines:")
     for headline in headlines:
-        print(headline.text)
+        print(headline.text)    # print out the content of headlines
+ 
+
+        # <div class="col-md-4 country
+    headlines = soup.find_all('span')
+    
+    print("country info:")
+    for headline in headlines:
+        print(f'{headline.text}')    # print out the content of country info
+
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
