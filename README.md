d94f1dbcf035872cdb3baaa89e692998f994d0d6
iawall <iawall@siue.edu>
2024-02-05 16:00:32 -0600

    retrieved first article, put it to an outfile

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ Firstly, I created my repository in git, linked it to VScode, installed packages
 
 After doing this, I then pathed my VScode to the miniconda environment so that the code i run in VScode uses the packages within my conda environment.
 
-I then created my scraping python program:
-  --  scrap.py explanation soon --
+I then created my scraping python program (step by step):
+  1. program imports request and BeautifulSoup
+  2. opens the specified file that will be written over with the scraped website
+  3. The code then sends a get request to the website being scraped. If the response status code = 200, then the request was successfully retrieved. Else, it prints out the response status code of why it failed
+  4. If successful, i use beautifulsoup to find all html specified under 'h1' or header 1 described html. i then write these headlines to the out file
+  5. after getting all h1 headers, i then retrieve all text with the 'p' or paragraph html and write it to the out file
 
 
 
--- scrap.py
+++ scrap.py
@@ -1,25 +1,24 @@
 import requests                 # Makes HTTP requests
 from bs4 import BeautifulSoup   # Web scraping
 
-url = 'https://www.scrapethissite.com/pages/simple/' # name of the site to scrape
-response = requests.get(url)    # Sends a GET request to the website
-
-if response.status_code == 200: # successful retrieval request
-    soup = BeautifulSoup(response.text, 'html.parser') # Parsing the content of the page
+with open('article1.txt', 'w') as file: # writes article to the specified filename
     
-    headlines = soup.find_all('h1')  # Assuming headlines are in h1 tags
-    print("Headlines:")
-    for headline in headlines:
-        print(headline.text)    # print out the content of headlines
+    url = 'https://www.foxnews.com/entertainment/kelly-clarksons-weight-loss-motivated-being-pre-diabetic' # name of the site to scrape
+    response = requests.get(url)    # Sends a GET request to the website
+
+    if response.status_code == 200: # successful retrieval request
+        soup = BeautifulSoup(response.text, 'html.parser') # Parsing the content of the page
+    
+        headlines = soup.find_all('h1')  # Assuming headlines are in h1 tags
+        file.write("Headlines:")
+        for headline in headlines:
+            file.write(headline.text)    # print out the content of headlines
  
-
-        # <div class="col-md-4 country
-    headlines = soup.find_all('span')
+        header = soup.find_all('p')  # getting paragraphs
+        file.write("Paragraphs")
+        for header in header:
+            file.write(header.text)    # print out the content of paragraphs
     
-    print("country info:")
-    for headline in headlines:
-        print(f'{headline.text}')    # print out the content of country info
-
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
